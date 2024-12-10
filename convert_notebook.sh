871bc25a8969bf82e2460e30cfe8eb2c121dedd1
Noah <38987002+OOS-Team@users.noreply.github.com>
2024-12-10 22:08:45 +0100

    Delete convert_notebook.sh

--- convert_notebook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install nbconvert if not already installed
-pip install nbformat
-
-# Convert the script to notebook
-python convert_to_notebook.py lung_cancer_detection.py lung_cancer_detection.ipynb
-
-echo "Conversion complete! The notebook has been created as lung_cancer_detection.ipynb"
